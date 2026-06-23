2064803a7eaec929f48e4bc5667a98336b37ac0f
sabrinafig <sabrinaffalmeida@gmail.com>
2026-06-23 16:12:06 -0300

    Update aluno_dao.py

--- GerenciadorNotas_Sabrina/dao/aluno_dao.py
+++ GerenciadorNotas_Sabrina/dao/aluno_dao.py
@@ -4,92 +4,61 @@ from model.Aluno import Aluno
 class AlunoDAO:
 
     def inserir(self, aluno):
-
         conexao = DatabaseConfig.conectar()
         cursor = conexao.cursor()
-
         sql = """
-        INSERT INTO aluno(nome, matricula)
-        VALUES (%s, %s)
+        INSERT INTO aluno(nome, matricula, email)
+        VALUES (%s, %s, %s)
         RETURNING id
         """
-
-        cursor.execute(
-            sql,
-            (aluno.nome, aluno.matricula)
-        )
-
+        cursor.execute(sql, (aluno.nome, aluno.matricula, aluno.email))
         aluno.id = cursor.fetchone()[0]
-
         conexao.commit()
-
         cursor.close()
         conexao.close()
-
 
     def listar(self):
-
         conexao = DatabaseConfig.conectar()
         cursor = conexao.cursor()
-
-        cursor.execute(
-            "SELECT id, nome, matricula FROM aluno"
-        )
-
+        cursor.execute("SELECT id, nome, matricula, email FROM aluno")
         alunos = []
-
         for linha in cursor.fetchall():
-
-            alunos.append(
-                Aluno(
-                    linha[1],  
-                    linha[2],  
-                    linha[0]   
-                )
-            )
-
+            alunos.append(Aluno(linha[1], linha[2], linha[3], linha[0]))
         cursor.close()
         conexao.close()
-
         return alunos
 
-
-    def atualizar(self, aluno):
-
+    def buscar_por_nome(self, nome_busca):
         conexao = DatabaseConfig.conectar()
         cursor = conexao.cursor()
+        sql = "SELECT id, nome, matricula, email FROM aluno WHERE nome ILIKE %s"
+        cursor.execute(sql, (f"%{nome_busca}%",))
+        alunos = []
+        for linha in cursor.fetchall():
+            alunos.append(Aluno(linha[1], linha[2], linha[3], linha[0]))
+        cursor.close()
+        conexao.close()
+        return alunos
 
+    def atualizar(self, aluno):
+        conexao = DatabaseConfig.conectar()
+        cursor = conexao.cursor()
         cursor.execute(
             """
             UPDATE aluno
-            SET nome=%s,
-                matricula=%s
+            SET nome=%s, matricula=%s, email=%s
             WHERE id=%s
             """,
-            (
-                aluno.nome,
-                aluno.matricula,
-                aluno.id
-            )
+            (aluno.nome, aluno.matricula, aluno.email, aluno.id)
         )
-
         conexao.commit()
-
         cursor.close()
         conexao.close()
 
-
     def excluir(self, id_aluno):
-
         conexao = DatabaseConfig.conectar()
         cursor = conexao.cursor()
-
-        cursor.execute(
-            "DELETE FROM aluno WHERE id=%s",
-            (id_aluno,)
-        )
-
+        cursor.execute("DELETE FROM aluno WHERE id=%s", (id_aluno,))
         conexao.commit()
-
         cursor.close()
         conexao.close()
