9fa08eb5cc3eeae334e599148d1eae37e16260bc
sabrinafig <sabrinaffalmeida@gmail.com>
2026-06-23 16:09:55 -0300

    Update Professor.py

--- GerenciadorNotas_Sabrina/model/Professor.py
+++ GerenciadorNotas_Sabrina/model/Professor.py
@@ -1,8 +1,9 @@
 from .Pessoa import Pessoa
 
 class Professor(Pessoa):
-    def __init__(self, nome, titulo):
+    def __init__(self, nome, titulo, id=None):
         super().__init__(nome)
+        self.id = id
         self.titulo = titulo
 
     def exibir_dados(self):
